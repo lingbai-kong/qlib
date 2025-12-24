eff9b2f0c735bd53883736c919abb0a9aaf823b9
konglingbai <konglinghundred@gmail.com>
2025-12-24 21:31:28 +0800

    [feat] AlphaX handler

--- qlib/contrib/data/handler.py
+++ qlib/contrib/data/handler.py
@@ -193,3 +193,49 @@ class Alpha158RewardShaping(Alpha158):
 class Alpha158vwap(Alpha158):
     def get_label_config(self):
         return ["Ref($vwap, -2)/Ref($vwap, -1) - 1"], ["LABEL0"]
+    
+class AlphaX(DataHandlerLP):
+    def __init__(
+        self,
+        instruments="csi500",
+        start_time=None,
+        end_time=None,
+        freq="day",
+        infer_processors=_DEFAULT_INFER_PROCESSORS,
+        learn_processors=_DEFAULT_LEARN_PROCESSORS,
+        fit_start_time=None,
+        fit_end_time=None,
+        filter_pipe=None,
+        inst_processors=None,
+        **kwargs,
+    ):
+        infer_processors = check_transform_proc(infer_processors, fit_start_time, fit_end_time)
+        learn_processors = check_transform_proc(learn_processors, fit_start_time, fit_end_time)
+
+        data_loader = {
+            "class": "QlibDataLoader",
+            "kwargs": {
+                "config": {
+                    "feature": kwargs.pop("feature"),
+                    "label": kwargs.pop("label", self.get_label_config(kwargs.pop("label_config", {}))),
+                },
+                "filter_pipe": filter_pipe,
+                "freq": freq,
+                "inst_processors": inst_processors,
+            },
+        }
+
+        super().__init__(
+            instruments=instruments,
+            start_time=start_time,
+            end_time=end_time,
+            data_loader=data_loader,
+            learn_processors=learn_processors,
+            infer_processors=infer_processors,
+            **kwargs,
+        )
+
+    def get_label_config(self, label_config={}):
+        future_window = label_config.get("future_window", 1)
+        assert future_window >= 1, "future_window should be at least 1"
+        return [f"Ref($close, -{1 + future_window})/Ref($close, -1) - 1"], ["LABEL0"]
